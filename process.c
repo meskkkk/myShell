9c97d373cd4ea8b34cec7a08b50d7ec0cfff7e4e
Shahd-Fawzy <shahdtoutou31@gmail.com>
2026-04-18 21:24:58 +0000

    part 3 is finished

--- process.c
+++ process.c
@@ -1,6 +1,34 @@
 #include "shell.h"
 
 void execute_command(Command *cmd) {
-    // Temporary stub — Member 3 replaces this
-    printf("[process stub] would execute: %s\n", cmd->args[0]);
+    if (cmd == NULL || cmd->arg_count == 0){
+       return;
+    }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return;
+    }
+    else if (pid == 0) {
+        if (cmd->input_file != NULL || cmd->output_file != NULL) {
+           execute_with_redirection(cmd);
+           exit(0);
+        }
+        else {
+             if (execvp(cmd->args[0],cmd->args) == -1) {
+                fprintf(stderr,"myShell: %s: command not found\n",cmd->args[0]);
+                exit(EXIT_FAILURE);
+             }
+        }
+    }
+    else {
+        if(cmd->is_background) {
+           printf ("[Background] PID: %d\n", pid);
+        }
+        else {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
 }
+
