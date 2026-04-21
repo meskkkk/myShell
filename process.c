2047052e7276590edc3f3dd7d3048e9f0cfa3878
Shahd-Fawzy <shahdtoutou31@gmail.com>
2026-04-21 17:39:44 +0000

    Fianl fix process.c -remove double fork bug

--- process.c
+++ process.c
@@ -1,7 +1,9 @@
 #include "shell.h"
 
 void execute_command(Command *cmd) {
-    if (cmd == NULL || cmd->arg_count == 0) return;
+    if (cmd == NULL || cmd->arg_count == 0) {
+        return;
+    }
 
     pid_t pid = fork();
 
@@ -12,18 +14,19 @@ void execute_command(Command *cmd) {
 
     if (pid == 0) {
         if (cmd->input_file != NULL || cmd->output_file != NULL) {
-            execute_with_redirection(cmd);
-        } else {
-            if (execvp(cmd->args[0], cmd->args) == -1) {
-                fprintf(stderr, "myShell: %s: command not found\n", cmd->args[0]);
-                exit(EXIT_FAILURE);
-            }
+            setup_redirection(cmd);
+        }
+
+        if (execvp(cmd->args[0], cmd->args) == -1) {
+            fprintf(stderr, "myShell: %s: command not found\n", cmd->args[0]);
+            exit(EXIT_FAILURE);
         }
     } 
     else {
         if (cmd->is_background) {
             printf("[Background] PID: %d\n", pid);
-        } else {
+        } 
+        else {
             int status;
             waitpid(pid, &status, 0);
         }
